ed52a55fb9aba5739a1b7a938000a31ad6e4733c
Romuald CARI <romuald.cari@gmail.com>
2012-06-04 17:19:25 +0200

    Cleaner module inclusion, activated build for Nigel.

--- source/build/CMake/platforms/MacOS.cmake
+++ source/build/CMake/platforms/MacOS.cmake
@@ -2,7 +2,12 @@
 SET( CMAKE_OSX_ARCHITECTURES "i386;x86_64" )
 
 # Compilation additional flags
-SET( CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} "-Wall -Werror" )
+IF( CMAKE_COMPILER_IS_GNUCXX ) 
+	SET( CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} "-Wall -Werror" )
+ELSE()
+	# This one is because Clang sucks!
+	SET( CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} "-Wall -Werror -Wno-overloaded-virtual" )
+ENDIF( CMAKE_COMPILER_IS_GNUCXX )
 
 # Platform definitions
 ADD_DEFINITIONS( -D_K_MACX )
